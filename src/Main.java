80cc28e5f83453f457f278c9d40aa94ee4daba61
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-06 18:30:46 +0200

    upd

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     // информация об игроке
-    static String name = "";
+    private static String name = "";
     // информация о противнике
-    static String enemyName = "";
+    private static String enemyName = "";
     // информация о сокровище
-    static String treasureName = "";
-    static Scanner in = new Scanner(System.in);
+    private static String treasureName = "";
+    private static Scanner in = new Scanner(System.in);
 
-    static String room1 = "Ты проник через вход в пещеры.";
-    static String room2 = "Ты продвигаешься дальше, вглубь пещеры.";
-    static String room3 = "Ты доcтиг предельных глубин пещеры.";
+    private static String room1 = "Ты проник через вход в пещеры.";
+    private static String room2 = "Ты продвигаешься дальше, вглубь пещеры.";
+    private static String room3 = "Ты доcтиг предельных глубин пещеры.";
 
     public static void main(String[] args) {
+        if (intro()) System.exit(0);
         treasureName = "Золотой меч";
         enemyName = "гоблина";
         room(true, true, room1);
@@ -24,11 +25,11 @@ public class Main {
         room(true, true, room3);
     }
 
-    private boolean intro() {
+    private static boolean intro() {
         System.out.println("О, храбрый рыцарь! Как твоё имя?");
         name = in.next();
         System.out.printf("Нам нужна твоя помощь, %s, нашу деревню захватили гоблины из северных пещер.%n", name);
-        System.out.println("Примешь ли ты вызов? %n1) да%n2) нет%n");
+        System.out.println("Примешь ли ты вызов? \n1) да\n2) нет");
         int response = in.nextInt();
         return !(response == 1);
     }
@@ -42,7 +43,7 @@ public class Main {
                 System.out.println("Что ты желаешь сделать?");
                 if (enemy) {
                     System.out.printf("1) Напасть на подлого %s%n", enemyName);
-                } else if (!enemy) {
+                } else {
                     System.out.println("1) Перейти в следующую комнату.");
                 }
                 if (treasure) {
@@ -55,9 +56,7 @@ public class Main {
                     if (enemy) {
                         enemy = !enemy;
                         System.out.printf("Ты убил смертоносного %s.%n", enemyName);
-                    } else if (!enemy) {
-                        return;
-                    }
+                    } else return;
                     break;
                 case 2:
                     treasure = !treasure;
